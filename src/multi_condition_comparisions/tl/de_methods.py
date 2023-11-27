da2b4416b2e34736a290ab262788810f1675774c
Emma Dann <ed6@sanger.ac.uk>
2023-11-27 12:11:22 +0000

    skeleton functions

---
+++ src/multi_condition_comparisions/tl/de_methods.py
@@ -0,0 +1,54 @@
+from typing import Union, Optional, Sequence, Tuple, Iterable, Dict, Any
+import numpy as np
+import pandas as pd
+
+from anndata import AnnData
+from numpy.typing import ArrayLike
+
+
+def _run_de_pydeseq2(
+        adata: AnnData,
+        design: Union[str, ArrayLike], 
+        contrast: str,      
+    ) -> pd.DataFrame:
+    '''
+    Run differential expression using pydeseq2.
+
+    Params:
+    -------
+    adata: AnnData
+        Annotated data matrix.
+    design: Union[str, ArrayLike]
+        Design. Either a matrix with the same number of rows as adata.X or a string of the design formula, or .
+    contrast: str
+        Contrast to test. Must be a string of the form 'condition1 - condition2'.
+
+    Returns:
+    --------
+    pd.DataFrame
+        Differential expression results
+    '''
+
+
+def _run_de_deseq2(
+        adata: AnnData,
+        design: Union[str, ArrayLike], 
+        contrast: str,      
+    ) -> pd.DataFrame:
+    '''
+    Run differential expression using pydeseq2.
+
+    Params:
+    -------
+    adata: AnnData
+        Annotated data matrix.
+    design: Union[str, ArrayLike]
+        Design. Either a matrix with the same number of rows as adata.X or a string of the design formula, or .
+    contrast: str
+        Contrast to test. Must be a string of the form 'condition1 - condition2'.
+
+    Returns:
+    --------
+    pd.DataFrame
+        Differential expression results
+    '''
