8b0e9d347a5513e552f0b9445b2ccf741fce9adf
Boris MUZELLEC <boris.muzellec@owkin.com>
2023-11-28 17:01:10 +0100

    test: add tests on results dataframe

--- tests/test_de.py
+++ tests/test_de.py
@@ -43,8 +43,8 @@ def test_adata():
     ],
 )
 def test_de(test_adata, method_class: BaseMethod, kwargs):
-    """Check that the method can be initialized and fitted and that the test_contrast
-    method returns a dataframe with the correct number of rows"""
+    """Check that the method can be initialized and fitted, and perform basic checks on
+    the result of test_contrasts."""
     method = method_class(adata=test_adata, design="~condition")
     method.fit(**kwargs)
     res_df = method.test_contrasts(np.array([0, 1]))
@@ -52,3 +52,7 @@ def test_de(test_adata, method_class: BaseMethod, kwargs):
     assert len(res_df) == test_adata.n_vars
     # Check that the index of the result matches the var_names of the adata
     tm.assert_index_equal(test_adata.var_names, res_df.index, check_order=False, check_names=False)
+    # Check that there is a p-value column
+    assert "pvalue" in res_df.columns
+    # Check that p-values are between 0 and 1
+    assert np.all((0 <= res_df["pvalue"]) & (res_df["pvalue"] <= 1))
