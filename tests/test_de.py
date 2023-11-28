9aabb7a209ebab1b75fd8305dfe2b3136839bc56
Gregor Sturm <mail@gregor-sturm.de>
2023-11-28 12:28:46 +0100

    Fix tests

--- tests/test_de.py
+++ tests/test_de.py
@@ -4,7 +4,7 @@ import pytest
 from pydeseq2.utils import load_example_data
 
 import multi_condition_comparisions
-from multi_condition_comparisions.tl.de import BaseMethod
+from multi_condition_comparisions.tl.de import BaseMethod, StatsmodelsDE
 
 
 def test_package_has_version():
@@ -28,11 +28,11 @@ def test_adata():
     return ad.AnnData(X=counts, obs=metadata)
 
 
-@pytest.param("method_class", [])
-def test_de(method_class: BaseMethod):
+@pytest.mark.parametrize("method_class", [StatsmodelsDE])
+def test_de(test_adata, method_class: BaseMethod):
     """Check that the method can be initialized and fitted and that the test_contrast
     method returns a dataframe with the correct number of rows"""
     method = method_class(adata=test_adata, design="~condition")
     method.fit()
-    res_df = method.test_contrast(np.array([0, 1]))
+    res_df = method.test_contrasts(np.array([0, 1]))
     assert len(res_df) == test_adata.n_vars
