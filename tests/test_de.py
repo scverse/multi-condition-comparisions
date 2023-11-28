04ae3d767df51f9ae72df934f02924ce4d41815e
Boris MUZELLEC <boris.muzellec@owkin.com>
2023-11-28 11:27:26 +0100

    test: define test_adata fixture

--- tests/test_de.py
+++ tests/test_de.py
@@ -1,10 +1,30 @@
+import anndata as ad
 import pytest
+from pydeseq2.utils import load_example_data
 
 import multi_condition_comparisions
 
 
 def test_package_has_version():
     assert multi_condition_comparisions.__version__ is not None
+
+
+@pytest.fixture
+def test_adata():
+    counts = load_example_data(
+        modality="raw_counts",
+        dataset="synthetic",
+        debug=False,
+    )
+
+    metadata = load_example_data(
+        modality="metadata",
+        dataset="synthetic",
+        debug=False,
+    )
+
+    return ad.AnnData(X=counts, obs=metadata)
+
 
 @pytest.param("method")
 def test_de(method):
