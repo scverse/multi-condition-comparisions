05f0831e4506e36dbf145b39a7fb1d87bc572b7a
Gregor Sturm <mail@gregor-sturm.de>
2023-11-28 09:17:22 +0100

    Stub methods for creating contrasts

--- src/multi_condition_comparisions/tl/de.py
+++ src/multi_condition_comparisions/tl/de.py
@@ -3,34 +3,36 @@ from typing import Literal
 
 import numpy as np
 import pandas as pd
+import patsy
 from anndata import AnnData
 
 
 class DeMethod(ABC):
-    def __init__(self, adata: AnnData):
+    def __init__(self, adata: AnnData, design: str | np.ndarray | Literal["patsy"], mask: str):
         """
         Initialize the method
 
         Parameters
         ----------
         adata
-            AnnData object, usually pseudobulked
+            AnnData object, usually pseudobulked.
+        design
+            Model design. Can be either a design matrix, a patsy formula.
+        mask
+            a column in adata.var that contains a boolean mask with selected features.
         """
         self.adata = adata
+        self.design = patsy.dmmatrix(design, adata.obs)
 
     @abstractmethod
-    def fit(self, design: str | np.ndarray | Literal["patsy"], mask: str, **kwargs) -> None:
+    def fit(self, **kwargs) -> None:
         """
         Fit the model
 
         Parameters
         ----------
-        design
-            Model design. Can be either a design matrix, a patsy formula
-        mask
-            a column in adata.var that contains a boolean mask with selected features
         **kwargs
-            Additional arguments for the specific method
+            Additional arguments for fitting the specific method.
         """
         pass
 
@@ -48,6 +50,8 @@ class DeMethod(ABC):
             either a single contrast, or a dictionary of contrasts where the key is the name for that particular contrast.
             Each contrast can be either a vector of coefficients (the most general case), a string, or a some fancy DSL
             (details still need to be figured out).
+
+            or a tuple withe three elements contrasts = ("condition", "control", "treatment")
         """
         for name, contrast in contrasts.items():
             self._test_single_contrast(contrast, **kwargs)
@@ -55,6 +59,11 @@ class DeMethod(ABC):
     def test_reduced(self, modelB: "DeMethod") -> pd.DataFrame:
         """
         Test against a reduced model
+
+        Parameters
+        ----------
+        modelB:
+            the reduced model against which to test.
 
         Example:
         --------
@@ -65,3 +74,32 @@ class DeMethod(ABC):
         ```
         """
         raise NotImplementedError
+
+    def cond(self, **kwargs) -> np.ndarray:
+        """
+        The intention is to make contrasts using this function as in glmGamPoi
+
+        >>> res <- test_de(fit, contrast = cond(cell = "B cells", condition = "stim") - cond(cell = "B cells", condition = "ctrl"))
+
+        Parameters
+        ----------
+        **kwargs
+
+        """
+        for factor, factor_info in self.design.design_info.factor_infos.items():
+            pass
+
+        patsy.build_design_matrices(self.design.design_info, kwargs)
+        raise NotImplementedError
+
+    def contrast(self, column: str, baseline: str, group_to_compare: str) -> np.ndarray:
+        """
+        Build a simple contrast for pairwise comparisons.
+
+        This is equivalent to
+
+        ```
+        model.cond(<column> = baseline) - model.cond(<column> = group_to_compare)
+        ```
+        """
+        return self.cond(**{column: baseline}) - self.cond(**{column: group_to_compare})
