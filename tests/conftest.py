9cc2f4befd40552c84a9a01992fffd8c28afed63
Boris MUZELLEC <boris.muzellec@owkin.com>
2023-11-29 11:38:06 +0100

    test: refactor fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,17 +4,23 @@ from pydeseq2.utils import load_example_data
 
 
 @pytest.fixture
-def test_adata():
-    counts = load_example_data(
+def test_counts():
+    return load_example_data(
         modality="raw_counts",
         dataset="synthetic",
         debug=False,
     )
 
-    metadata = load_example_data(
+
+@pytest.fixture
+def test_metadata():
+    return load_example_data(
         modality="metadata",
         dataset="synthetic",
         debug=False,
     )
 
-    return ad.AnnData(X=counts, obs=metadata)
+
+@pytest.fixture
+def test_adata(test_counts, test_metadata):
+    return ad.AnnData(X=test_counts, obs=test_metadata)
