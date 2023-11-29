c1233a874dc6ae97e32ef43da1c9d642b30389d1
Holly Whitfield <hw12@sanger.ac.uk>
2023-11-29 08:42:55 +0000

    fix conflicts

--- tests/test_de.py
+++ tests/test_de.py
@@ -1,6 +1,8 @@
 import anndata as ad
 import numpy as np
 import pytest
+import statsmodels.api as sm
+from pandas import testing as tm
 from pydeseq2.utils import load_example_data
 
 import multi_condition_comparisions
@@ -28,11 +30,29 @@ def test_adata():
     return ad.AnnData(X=counts, obs=metadata)
 
 
-@pytest.mark.parametrize("method_class", [StatsmodelsDE])
-def test_de(test_adata, method_class: BaseMethod):
-    """Check that the method can be initialized and fitted and that the test_contrast
-    method returns a dataframe with the correct number of rows"""
+@pytest.mark.parametrize(
+    "method_class,kwargs",
+    [
+        # OLS
+        (StatsmodelsDE, {}),
+        # Negative Binomial
+        (
+            StatsmodelsDE,
+            {"regression_model": sm.GLM, "family": sm.families.NegativeBinomial()},
+        ),
+    ],
+)
+def test_de(test_adata, method_class: BaseMethod, kwargs):
+    """Check that the method can be initialized and fitted, and perform basic checks on
+    the result of test_contrasts."""
     method = method_class(adata=test_adata, design="~condition")
-    method.fit()
+    method.fit(**kwargs)
     res_df = method.test_contrasts(np.array([0, 1]))
+    # Check that the result has the correct number of rows
     assert len(res_df) == test_adata.n_vars
+    # Check that the index of the result matches the var_names of the adata
+    tm.assert_index_equal(test_adata.var_names, res_df.index, check_order=False, check_names=False)
+    # Check that there is a p-value column
+    assert "pvalue" in res_df.columns
+    # Check that p-values are between 0 and 1
+    assert np.all((0 <= res_df["pvalue"]) & (res_df["pvalue"] <= 1))
