fb9d6e1e0d41108e2e6f06f204d43a796fe261fd
Boris MUZELLEC <boris.muzellec@owkin.com>
2023-11-29 11:42:05 +0100

    test: check that invalid count inputs raise ValueErrors in MethodBase

---
+++ tests/test_edge_cases.py
@@ -0,0 +1,14 @@
+import anndata as ad
+import numpy as np
+import pytest
+
+from multi_condition_comparisions.tl.de import StatsmodelsDE
+
+
+@pytest.mark.parametrize("invalid_input", [-1, 0.1, np.nan, np.inf])
+def test_invalid_inputs(invalid_input, test_counts, test_metadata):
+    """Check that invalid inputs in MethodBase counts raise an error."""
+    test_counts[0, 0] = invalid_input
+    adata = ad.AnnData(X=test_counts, obs=test_metadata)
+    with pytest.raises(ValueError):
+        StatsmodelsDE(adata=adata, design="~condition")
