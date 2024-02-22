f233f35e6c3845889a1238bd7ab4a2879d05c75e
Gregor Sturm <mail@gregor-sturm.de>
2024-02-22 11:03:23 +0100

    Add minimal test dataset

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
 import anndata as ad
+import pandas as pd
+import numpy as np
 import pytest
 from pydeseq2.utils import load_example_data
 
@@ -26,6 +28,36 @@ def test_metadata():
 @pytest.fixture
 def test_adata(test_counts, test_metadata):
     return ad.AnnData(X=test_counts, obs=test_metadata)
+
+
+@pytest.fixture
+def test_adata_minimal():
+    obs = pd.DataFrame(
+        [
+            ["A", "D1", "X", 0.2],
+            ["B", "D1", "X", 0.7],
+            ["A", "D2", "Y", 1.6],
+            ["B", "D2", "Y", 42],
+            ["A", "D3", "Y", 212],
+            ["B", "D3", "Y", 6023],
+        ],
+        columns=["condition", "donor", "other", "cont"],
+    )
+    var = pd.DataFrame(index=["gene1", "gene2"])
+    X = np.array(
+        [
+            # gene1 differs between condition A/B by approx. FC=2
+            # gene2 differs between donor D1/D2 by approx FC=2
+            # [gene1, gene2]
+            [10, 200],
+            [20, 220],
+            [12, 400],
+            [25, 420],
+            [13, 600],
+            [24, 620],
+        ]
+    )
+    return ad.AnnData(X=X, obs=obs, var=var)
 
 
 @pytest.fixture
